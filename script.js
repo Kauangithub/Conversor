dc9c9b92ae8dfe8dd7f689073d2d79c450c6a50a
Kauan Machado <machadob.kauan@gmail.com>
2025-08-05 21:51:58 -0300

    Script atualizado para reagir ao change

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 // console.log("Conversão iniciada") // Descomente para depuração
 
+let num = document.getElementById('num')
+
+num.addEventListener('input', Conversor)
+
 let botao = document.getElementById('btn')
 botao.addEventListener('click', Conversor)
+
+
 
 let result = document.getElementById('resultado')
 
 function Conversor(event) {
-    event.preventDefault()
     let num = parseFloat(document.getElementById('num').value)
-    let resto;
-    let binario = []
-    if (num != '') {
-        while (num != 0) {
-            resto = Math.trunc(num % 2).toString();
-            console.log(resto);
-            num = Math.trunc(num / 2);
-            binario.push(resto)
-            
-        }
-        if (resto !== undefined) {
-            result.textContent = resto;
-        } 
-        console.log(binario)
-        let binario2 = binario.reverse().join("")
-        result.textContent =  binario2
-        console.log(binario2)
-        console.log(typeof(binario))
-        
 
+    if (isNaN(num)) {
+        result.textContent = ''
     } else {
-        alert('insira números válidos')
-    }   
+        if (num === 0) {
+            result.textContent = 0
+        } else {
+            event.preventDefault()
+        let resto;
+        let binario = []
+        if (typeof(num) === 'number') {
+            while (num > 0) {
+                resto = Math.trunc(num % 2).toString();
+                console.log(resto);
+                num = Math.trunc(num / 2);
+                binario.push(resto)
+                
+            }
+            if (resto !== undefined) {
+                result.textContent = resto;
+            } 
+            console.log(binario)
+            let binario2 = binario.reverse().join("")
+            result.textContent =  binario2
+            console.log(binario2)
+            console.log(typeof(binario))
+            
+
+        } else {
+            alert('insira números válidos')
+        }  
+ 
+    }
+    
+        }
+        
 }
